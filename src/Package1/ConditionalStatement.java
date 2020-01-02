fcc8f9945d57747f41e02576a91c27034ae27dab
Mareeha Ferdowsi <mareehaferdowsi@Mareehas-MBP.home>
2020-01-02 03:58:11 -0500

    Third upload

---
+++ src/Package1/ConditionalStatement.java
@@ -0,0 +1,103 @@
+//package Package1;
+//
+//public class ConditionalStatement {
+//
+//    public static void main(String[] args) {
+
+//        if (condition) {
+//            // statement
+//        } else if (anotherCondition) {
+//                // Statement
+//        } else {
+//            //Default task
+//        }
+//     ___________________________________________________________________
+
+        // If hour is between 0600 and 1159: Greet Good Morning
+        // If hour is between 1200 and 1759: Greet Good Evening
+        // Good Night
+
+//        int hour = 1900;
+//
+//        if (hour >= 0600 && hour < 1200) {
+//            System.out.println("Good Morning");
+//        } else if (hour >= 1200 && hour < 1800) {
+//            System.out.println("Good Evening");
+//        } else {
+//            System.out.println("Good Night");
+//        }
+
+//     ___________________________________________________________________
+
+        /*
+// Classwork:
+
+        int temp = 45;
+        //If temp is 32 or lower then print "It is freezing outside!"
+        //If temp is 100 or higher then print  "It is hot outside"
+        //If above two statement is false then print "Got fot it, It is pretty outside"
+ */
+
+//        int temp = 45;
+//        if (temp <= 32) {
+//            System.out.println("It is freezing outside!");
+//        } else if (temp >= 100) {
+//            System.out.println("It is hot outside");
+//        } else {
+//            System.out.println("Got fot it, It is pretty outside");
+//        }
+
+//     ___________________________________________________________________
+
+//        //1. If customer has $1000 or more on the checking account and iPhone X is
+//        //Available on the inventory then display alert saying "You can goto store and purchase the iPhone"
+//        //2. If customer has credit score greater than 600 or customer has $1000
+//        // or more on checking account then display alert saying "You are eligible to purchase the phone",
+//        //3. if none of the above condition is not true then display alert saying "Your not eligible for iPhone"
+//
+//        int customerCheckingBalance = 1000;
+//        int iPhoneInventory = 10;
+//        int customerCreditScore = 700;
+//        if (customerCheckingBalance >=1000 && iPhoneInventory > 0) {
+//            System.out.println("You can purchase iPhone");
+//        } else if (customerCreditScore > 600 || customerCheckingBalance >= 1000) {
+//            System.out.println("You are eligible");
+//        } else {
+//            System.out.println("Your not eligible for iPhone");
+//        }
+
+//     ___________________________________________________________________
+
+// Classwork:
+
+//        String month = "January";
+//
+//        switch (month) {
+//            case "December":
+//            case "January":
+//            case "February":
+//                System.out.println("Winter");
+//                break;
+//            case "March":
+//            case "April":
+//            case "May":
+//                System.out.println("Spring");
+//                break;
+//            case "June":
+//            case "July":
+//            case "August":
+//                System.out.println("Summer");
+//                break;
+//            case "September":
+//            case "October":
+//            case "November":
+//                System.out.println("Fall");
+//                break;
+//            default:
+//                System.out.println("Invalid Month");
+//        }
+//
+//
+//
+//
+//    }
--- src/Package1/HW_Temp.java
+++ src/Package1/HW_Temp.java
@@ -1,29 +1,29 @@
-package Package1;
-
-public class HW_Temp {
-
-    public static void main(String[] args) {
-
-        double celsius1 = 25;
-
-        // Celsius to Kelvin
-        double kelvin1 = celsius1 + 273.15;
-        System.out.println(kelvin1);
-
-        // Celsius to Fahrenheit
-        double fahrenheit1 = (celsius1 * 9) / 5 + 32;
-        System.out.println(fahrenheit1);
-
-        double kelvin = 300;
-
-        // Kelvin to Celsius
-        double celsius = kelvin - 273.15;
-        System.out.println(celsius);
-
-        // Kelvin to Fahrenheit
-        double fahrenheit = (kelvin * 9) / 5 - 459.57;
-        System.out.println(fahrenheit);
-
-
-    }
-}
+//package Package1;
+//
+//public class HW_Temp {
+//
+//    public static void main(String[] args) {
+//
+//        double celsius1 = 25;
+//
+//        // Celsius to Kelvin
+//        double kelvin1 = celsius1 + 273.15;
+//        System.out.println(kelvin1);
+//
+//        // Celsius to Fahrenheit
+//        double fahrenheit1 = (celsius1 * 9) / 5 + 32;
+//        System.out.println(fahrenheit1);
+//
+//        double kelvin = 300;
+//
+//        // Kelvin to Celsius
+//        double celsius = kelvin - 273.15;
+//        System.out.println(celsius);
+//
+//        // Kelvin to Fahrenheit
+//        double fahrenheit = (kelvin * 9) / 5 - 459.57;
+//        System.out.println(fahrenheit);
+//
+//
+//    }
+//}
