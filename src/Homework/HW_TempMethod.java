df3e1042b473cbfb3f85f01a5a07ab72b1396387
Mareeha Ferdowsi <mareehaferdowsi@Mareehas-MacBook-Pro.local>
2020-01-13 00:38:55 -0500

    Sixth upload

---
+++ src/Homework/HW_TempMethod.java
@@ -0,0 +1,32 @@
+package Homework;
+
+public class HW_TempMethod {
+
+    public static void main(String[] args) {
+        System.out.println(getKelvin(75));
+        System.out.println(getFahrenheit(75));
+        System.out.println(getCelsius(75));
+        System.out.println(getFahrenheit1(75));
+    }
+
+    public static double getKelvin(double celsius) {
+        double kelvin = celsius +273.15;
+        return kelvin;
+    }
+
+    public static double getFahrenheit(double celsius) {
+        double Fahrenheit = (celsius * 9) / 5 + 32;
+        return Fahrenheit;
+    }
+
+    public static double getCelsius (double kelvin) {
+        double celsius = (kelvin -273.15);
+        return celsius;
+    }
+
+    public static double getFahrenheit1 (double kelvin) {
+        double Fahrenheit = (kelvin * 9) / 5 - 459.57;
+        return Fahrenheit;
+
+    }
+}
